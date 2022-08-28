3b11b2b7accfd51de5855a3ea8cf00471679c18f
teruteru <tutituti3080@gmail.com>
2022-08-28 20:59:46 +0900

    Add: 診断画面を作成。

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,3 @@
+class PagesController < ApplicationController
+  def diagnose; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
+  get '/diagnose' => 'pages#diagnose'
+  resources :districts do
+    resources :towns
+  end
 end
