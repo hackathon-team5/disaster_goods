0a49a2f82f716f5e56c07dc86ff391e3f8611d01
shuhei-T <nofuture742617000027@gmail.com>
2022-08-30 00:11:59 +0900

    feet: chart.jsの導入

--- app/controllers/diagnosis_controller.rb
+++ app/controllers/diagnosis_controller.rb
@@ -6,5 +6,8 @@ class DiagnosisController < ApplicationController
 
   def result
     @rank = Rank.find_by(town_id: params[:town_id])
+    @fire_danger_rank = @rank.fire_danger_rank.to_json.html_safe
+    @building_collapse_rank = @rank.building_collapse_rank.to_json.html_safe
+    @active_difficulty_rank = @rank.active_difficulty_rank.to_json.html_safe
   end
 end
