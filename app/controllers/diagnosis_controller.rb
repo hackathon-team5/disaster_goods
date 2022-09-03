59189577bc1080575a711ccb012bb59aaac68cfc
ooooose <yuuki.oose1990@gmail.com>
2022-09-03 17:34:03 +0900

    add: diagnosisコントローラ上の記載を修正

--- app/controllers/diagnosis_controller.rb
+++ app/controllers/diagnosis_controller.rb
@@ -13,46 +13,43 @@ class DiagnosisController < ApplicationController
     danger_list = [ @fire_danger_rank, @building_collapse_rank, @active_difficulty_rank]
     priority_list = [['fire', @fire_danger_rank], ['building', @building_collapse_rank], ['active', @active_difficulty_rank]]
     priority_count = equal_count(danger_list)
-    priority_ranks = priority_rank(priority_list, priority_count)
-    @types_id =
-    if priority_count ==1 && priority_ranks == ['fire']
-      1
-    elsif priority_count == 1 && priority_ranks == ['building']
-      2
-    elsif priority_count == 1 && priority_ranks == ['active']
-      3
-    elsif priority_count == 2 && priority_ranks.sort == ['fire', 'building'].sort
-      4
-    elsif priority_count == 2 && priority_ranks.sort == ['fire','active'].sort
-      5
-    elsif priority_count == 2 && priority_ranks.sort == ['building', 'active'].sort
-      6
-    elsif priority_count == 3
-      7
-    end
+    priority_ranks = rank_types(priority_list, priority_count)
+    @types_id = if (priority_count == 1) && (priority_ranks == ['fire'])
+                  1
+                elsif (priority_count == 1) && (priority_ranks == ['building'])
+                  2
+                elsif (priority_count == 1) && (priority_ranks == ['active'])
+                  3
+                elsif (priority_count == 2) && (priority_ranks.sort == ['building', 'fire'].sort)
+                  4
+                elsif (priority_count == 2) && (priority_ranks.sort == ['active', 'fire'].sort)
+                  5
+                elsif (priority_count == 2) && (priority_ranks.sort == ['active', 'building'].sort)
+                  6
+                elsif priority_count == 3
+                  7
+                end
   end
 
   private
 
-  def equal_count(list)
-    list.sort! {|x,y| y<=>x}
-    count = 1
-    pointed_at = 0
-    (list.length - 1).times do
-      if list[pointed_at] == list[pointed_at + 1]
-        count += 1
+  def equal_count(danger_list)
+    danger_list.sort! {|x,y| y<=>x}
+    priority_count = 1
+    (danger_list.length - 1).times do |n|
+      if danger_list[n] == danger_list[n + 1]
+        priority_count += 1
       else
         break
       end
-      pointed_at += 1
     end
-    count
+    priority_count
   end
 
-  def priority_rank(list, count)
-    list.sort! { |x,y| y[1]<=>x[1] }
+  def rank_types(priority_list, priority_count)
+    priority_list.sort! { |x,y| y[1]<=>x[1] }
     priority_disaster = []
-    list.first(count).each do |n| 
+    priority_list.first(priority_count).each do |n| 
       priority_disaster << n[0]
     end
     priority_disaster
