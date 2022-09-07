a6d0f2c9724250c91f192c04735b9c3cb36cdaa4
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-07 22:16:11 +0900

    fix:修正

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,6 +3,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = {
+    entry: {
+        entry: 'app/javascript/packs/application.js'
+      },
   module:{
     rules:[
         {
