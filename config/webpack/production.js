322120347213b69e661e6d924274bbd8cd7b1107
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-07 21:44:42 +0900

    fix:production.jsの更新

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,33 +2,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = 
-{
-  test: /\.(scss)$/,
-  use: [{
-    // inject CSS to page
-    loader: 'style-loader'
-  }, {
-    // translates CSS into CommonJS modules
-    loader: 'css-loader'
-  }, {
-    // Run postcss actions
-    loader: 'postcss-loader',
-    options: {
-      // `postcssOptions` is needed for postcss 8.x;
-      // if you use postcss 7.x skip the key
-      postcssOptions: {
-        // postcss plugins, can be exported to postcss.config.js
-        plugins: function () {
-          return [
-            require('autoprefixer')
-          ];
+module.exports = {
+  module:{
+    rules:[
+        {
+            test:/\.scss$/,
+            use:['style-loader','css-loader','sass-loader']
         }
-      }
+     ]
     }
-  }, {
-    // compiles Sass to CSS
-    loader: 'sass-loader'
-  }]
-}
-
+  }
