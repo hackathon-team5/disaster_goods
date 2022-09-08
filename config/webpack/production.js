894f1669a28c368fbe1d1f984667d35c762c512c
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-08 14:56:28 +0900

    fix:production.jsを元に戻す

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,16 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = {
-    entry: {
-        entry: 'app/javascript/packs/application.js'
-      },
-  module:{
-    rules:[
-        {
-            test:/\.scss$/,
-            use:['style-loader','css-loader','sass-loader']
-        }
-     ]
-    }
-  }
+module.exports = environment.toWebpackConfig()
