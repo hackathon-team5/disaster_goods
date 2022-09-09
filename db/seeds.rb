152ab566004e586040729c66c9d0e30f82eb2896
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-09 14:04:10 +0900

    fix:seeds.rbの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,14 @@ CSV.foreach('db/town.csv', headers: true) do |row|
         district_id: row['区市町名'])
 end
 
-# CSV.foreach('db/rank.csv', headers: true) do |row|
-#     Rank.create(
-#         total_danger_rank: row['総合危険度ランク'],
-#         fire_danger_rank: row['火災危険度ランク'],
-#         building_collapse_rank: row['建物倒壊危険度ランク'],
-#         active_difficulty_rank: row['災害時活動困難度ランク'],
-#         town_id: row['町丁目名'])
-# end
+CSV.foreach('db/rank.csv', headers: true) do |row|
+    Rank.create(
+        total_danger_rank: row['総合危険度ランク'],
+        fire_danger_rank: row['火災危険度ランク'],
+        building_collapse_rank: row['建物倒壊危険度ランク'],
+        active_difficulty_rank: row['災害時活動困難度ランク'],
+        town_id: row['町丁目名'])
+end
 
 # CSV.foreach('db/type.csv', headers: true) do |row|
 #     Type.create(
