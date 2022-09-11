00c1c71bb38687bfa2e69e4ee3bae82cd948ee93
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-11 12:33:02 +0900

    fix:typesとgoodsの関連性の追加

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,4 +1,6 @@
 class Type < ApplicationRecord
+    has_many :goods
+
     validates :name, presence: true
     validates :description, presence: true 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_31_062502) do
+ActiveRecord::Schema.define(version: 2022_09_11_032127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "districts", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "goods", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.string "item_code", null: false
+    t.string "amazon_link"
+    t.integer "type_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
