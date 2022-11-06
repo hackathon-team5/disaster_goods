3ee2e0209ba68e6e0f6db78678209130d64e81a2
ooooose <yuuki.oose1990@gmail.com>
2022-11-06 22:15:52 +0900

    add:UserSessionsコントローラとビューの整備

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,18 @@
+class UserSessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to rails_admin_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to login_path
+  end
+end
